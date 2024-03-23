4c1d2e8610fac686bc158341ee25f76ca8788c0f
Laura Haro Escoi <lauraha.escoi@gmail.com>
2024-03-23 18:01:50 +0100

    rocket and meteors working

---
+++ GameJam_2024/Assets/Scripts/MeteorMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MeteorMovement : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    public MeteorsScript spawner;
+    public DangerScript danger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner = GameObject.FindGameObjectWithTag("MeteorSpawner").GetComponent<MeteorsScript>();
+        danger = GameObject.FindGameObjectWithTag("dangerSign").GetComponent <DangerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 src = this.transform.position;
+        Vector2 dst = new Vector2(0, 0);
+        transform.position = Vector2.MoveTowards(src, dst, moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Planet"))
+        {
+            Destroy(this.gameObject);
+            spawner.destroyMeteor();
+            danger.destroySign();
+        }
+    }
+}
+    
--- GameJam_2024/Assets/Scripts/MeteorsScript.cs
+++ GameJam_2024/Assets/Scripts/MeteorsScript.cs
@@ -1,55 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline;
 using UnityEngine;
 
-public class MaterialBehavior : MonoBehaviour
+public class MeteorsScript : MonoBehaviour
 {
     // Start is called before the first frame update
     //Create a random object that spawns at a random location
-    public GameObject randomObject;
+    public GameObject meteor;
+    public GameObject dangerSign;
 
     //Create a spawn time for the object
     public float spawnTime;
 
-    public float radius;
+    public float radiusMeteor;
+    public float radiusDangerSign;
     //offset from the center of the sphere
     public Vector2 offset;
 
-    private float limitMinerals = 5;
+    private float limitMeteors = 1;
 
     private float counter = 0;
 
+   
     void Start()
     {
         //Set the spawn time to a random value between 5 and 10 seconds
-        spawnTime = Time.time + Random.Range(spawnTime - 2.0f, spawnTime + 2.0f);
+        spawnTime = Time.time + Random.Range(5.0f, 10.0f);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+       
         if (Time.time >= spawnTime)
         {
             //Spawn the object
             SpawnObject();
             //Reset the spawn time
-            spawnTime = Time.time + Random.Range(spawnTime - 2.0f, spawnTime + 2.0f);
+            spawnTime = Time.time + Random.Range(5.0f, 10.0f);
         }
     }
 
     public void SpawnObject()
-    {
+    {   
+        
         //Create a random object at a random location in the perimeter of a circle
-        if (counter < limitMinerals)
+        if (counter < limitMeteors)
         {
-            Vector2 spawnPosition = Random.insideUnitCircle.normalized * radius + offset;
-            Instantiate(randomObject, spawnPosition, Quaternion.identity);
+            var coor = Random.insideUnitCircle.normalized;
+            Vector2 spawnPositionMeteor = coor * radiusMeteor + offset;
+            Vector2 spawnPositionDanger = coor * radiusDangerSign + offset;
+            Instantiate(meteor, spawnPositionMeteor, Quaternion.identity);
+            Instantiate(dangerSign, spawnPositionDanger, Quaternion.identity);
             counter++;
         }
 
     }
 
-    public void pickMaterial()
+    public void destroyMeteor()
     {
         counter--;
     }
