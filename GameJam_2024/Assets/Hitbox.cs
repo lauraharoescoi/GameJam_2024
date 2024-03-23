b5889bd6a2443c9f74067b0c408d23207a18dc87
naimsg <naimsaadigallego@gmail.com>
2024-03-23 09:47:57 +0100

    Factory "finished" ?

--- GameJam_2024/Assets/Hitbox.cs
+++ GameJam_2024/Assets/Hitbox.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hitbox : MonoBehaviour
 {
     public FactoryScript factory;
+   
 
     private void Start()
     {
         factory = GameObject.FindGameObjectWithTag("FactoryLogic").GetComponent<FactoryScript>();
+     
     }
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (Input.GetKeyDown(KeyCode.E)){
+    { 
+        if (Input.GetKeyDown(KeyCode.E) /* && character has mineral */){
+
             // Personatje - 3 mineral
-            factory.addMineral();
+            if(factory.waiting)
+            {
+                factory.addMinerals();
+            }
         }
         
     }
